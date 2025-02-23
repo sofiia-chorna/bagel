60d42a89ab26d9e87a97a0485be5a9f0e8b6a844
Sofiia Chorna <chernaya.sofiia@gmail.com>
2025-02-23 01:14:07 +0100

    Add logger

---
+++ xai/utils/logger.py
@@ -0,0 +1,22 @@
+import time
+import os
+import logging
+from pathlib import Path
+
+
+def get_logger() -> logging.Logger:
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+
+    logs_dir = Path("logs")
+    logs_dir.mkdir(exist_ok=True)
+    log_file = os.path.join(logs_dir, f"run_{time.strftime('%Y-%m-%d_%H-%M-%S')}.log")
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setFormatter(
+        logging.Formatter(
+            "[%(asctime)s] %(levelname)s: %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
+        ),
+    )
+    logger.addHandler(file_handler)
+
+    return logger
