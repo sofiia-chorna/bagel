2fc2a35f56efb582f735d8cd2e0f96482598bd89
Sofiia Chorna <chernaya.sofiia@gmail.com>
2025-02-26 23:32:08 +0100

    Add dataset typing

--- xai/utils/consts.py
+++ xai/utils/consts.py
@@ -3,5 +3,6 @@ from typing import Literal
 import torch
 
 MODEL_NAMES = Literal["alexnet", "resnet18", "vgg16"]
+DATASET_TYPES = Literal["hugging_face", "local"]
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
