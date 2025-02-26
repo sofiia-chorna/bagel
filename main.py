788b672b1b02bbb8d006674963c4a9f21802f317
Sofiia Chorna <chernaya.sofiia@gmail.com>
2025-02-26 23:46:23 +0100

    Refactor

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-
 import click
-import torch
+from datasets import Dataset, load_dataset
 
 from xai.concepts.concept_manager import Concept_Manager
 from xai.concepts.multilabel_classification import run_multilabel_clf
@@ -9,7 +7,7 @@ from xai.datasets.datamodule import DataModule
 from xai.feature_extraction.feature_extraction import run_extract_features
 from xai.models.models import get_model
 from xai.utils.cli import path
-from xai.utils.file import save_json
+from xai.utils.file import save
 from xai.utils.logger import logger
 from xai.utils.params import Params
 
@@ -21,56 +19,71 @@ def main():
 
 @main.command()
 @path
-def extract_features(path: str):
+def annotate(path: str):
     params = Params.from_yaml(path)
     logger.info(f"Params used: {params.to_json()}")
 
-    loader = DataModule(params)
+    # dataset
+    train_ds: Dataset = load_dataset(params.dataset_name, split="train")  # type: ignore
+    val_ds: Dataset = load_dataset(params.dataset_name, split="validation")  # type: ignore
 
-    extract_feat_params = params.feature_extraction
-    if extract_feat_params is None:
-        logger.error("No 'feature_extraction' params provided")
-        exit(0)
+    # concepts
+    concept_manager = Concept_Manager()
 
-    model = get_model(extract_feat_params.model_name, loader.num_classes)
-    run_extract_features(model, loader)
+    train_concepts_df = concept_manager.ask_llm(train_ds, params.batch_size)
+    val_concepts_df = concept_manager.ask_llm(val_ds, params.batch_size)
 
-    logger.info("End!")
+    # TODO: implement
+    # concept_df = concept_manager.extract_concepts(computation_params.dataset)
+
+    print(type(train_concepts_df))
+    print(train_concepts_df)
+
+    save("pickle", f"concepts/{params.dataset_name}_train.pkl", train_concepts_df)
+    save("pickle", f"concepts/{params.dataset_name}_val.pkl", val_concepts_df)
 
 
 @main.command()
 @path
-def compute_multilabel_classification(path: str):
+def explain(path: str):
     params = Params.from_yaml(path)
     logger.info(f"Params used: {params.to_json()}")
 
-    computation_params = params.multilabel_classification
-    if computation_params is None:
-        logger.error("No 'computation_params' params provided")
+    # dataloaders
+    datamodule = DataModule(params.dataset_type, params.dataset_name, params.batch_size)
+
+    # concepts
+    concept_manager = Concept_Manager()
+
+    if params.train_concepts_path and params.val_concepts_path:
+        train_concepts_df = concept_manager.load(params.train_concepts_path)
+        val_concepts_df = concept_manager.load(params.val_concepts_path)
+    else:
+        logger.error(
+            "No 'train_concepts_path' or 'val_concepts_path' provided. Please run annotation first 'python3 main.py annotate'"
+        )
         exit(0)
 
-    features_dict = torch.load(computation_params.features_path, weights_only=True)
+    print(train_concepts_df.head(1)["concepts"])
 
-    concept_manager = Concept_Manager()
-    if computation_params.dataframe_path:
-        dataframe = concept_manager.load(computation_params.dataframe_path)
+    # calculate
+    for model_name in params.models:
+        model = get_model(model_name, datamodule.num_classes)
 
-    # TODO: implement
-    # elif computation_params.dataset:
-    # dataframe = concept_manager.extract_concepts(computation_params.dataset)
-    # elif computation_params.llm:
-    #    dataframe = concept_manager.ask_llm(computation_params.llm)
+        train_features = run_extract_features(model, datamodule.train_loader)
+        val_features = run_extract_features(model, datamodule.val_loader)
 
-    else:
-        raise ValueError(
-            "Please provide either 'dataframe_path', 'dataset' or 'llm' params"
+        base_name = f"{params.dataset_name}_{model.get_name()}"
+        save("torch", f"features/{base_name}_train.pt", train_features)
+        save("torch", f"features/{base_name}_val.pt", val_features)
+
+        results = run_multilabel_clf(
+            train_df=train_concepts_df,
+            val_df=val_concepts_df,
+            train_features_dict=train_features,
+            val_features_dict=val_features,
         )
-
-    output_filename = computation_params.output_filename
-    results = run_multilabel_clf(dataframe, features_dict)
-    save_json(os.path.join("results", f"{output_filename}.json"), results)
-
-    logger.info("End!")
+        save("json", f"results/{base_name}.json", results)
 
 
 if __name__ == "__main__":
--- xai/utils/params.py
+++ xai/utils/params.py
@@ -1,60 +1,25 @@
 import json
-from dataclasses import asdict, dataclass
-from typing import Literal, Optional
+from dataclasses import asdict, dataclass, field
+from typing import List, Literal, Optional
 
 import yaml
-
-from xai.utils.consts import MODEL_NAMES
-
-
-@dataclass
-class FeatureExtraction:
-    model_name: MODEL_NAMES = "alexnet"
-    dataset: Literal["cats_dogs_cars"] = "cats_dogs_cars"
-    batch_size: int = 64
-
-
-@dataclass
-class MultilabelClassification:
-    features_path: str = "features/features.pth"
-    dataframe_path: str = "dataframes/dataframe.pth"
-    output_filename: str = "results"
 
 
 @dataclass
 class Params:
-    feature_extraction: Optional[FeatureExtraction] = None
-    multilabel_classification: Optional[MultilabelClassification] = None
-
     # metadata
-    model_name: MODEL_NAMES = "alexnet"
-    dataset: Literal["cats_dogs_cars"] = "cats_dogs_cars"
     batch_size: int = 64
+    models: List[str] = field(default_factory=lambda: ["alexnet"])
+    dataset_type: Literal["hugging_face"] = "hugging_face"
+    dataset_name: str = "ENSTA-U2IS/Cats_Dogs_Cars"
+    train_concepts_path: Optional[str] = None
+    val_concepts_path: Optional[str] = None
 
     @classmethod
     def from_yaml(cls, path: str) -> "Params":
         with open(path, "r") as stream:
             params = yaml.safe_load(stream)
-
-            feature_extraction = None
-            multilabel_classification = None
-
-            # feature extraction params
-            feat_extraction_params = params.get("feature_extraction")
-            if feat_extraction_params:
-                feature_extraction = FeatureExtraction(**feat_extraction_params)
-
-            # multilabel classification params
-            multilabel_cls_params = params.get("multilabel_classification")
-            if multilabel_cls_params:
-                multilabel_classification = MultilabelClassification(
-                    **multilabel_cls_params
-                )
-
-            return cls(
-                feature_extraction=feature_extraction,
-                multilabel_classification=multilabel_classification,
-            )
+            return cls(**params)
 
     def to_json(self) -> str:
         filtered_dict = {
