ece0080fcad585c838099cc6d66bab1efc96ddca
Sofiia Chorna <chernaya.sofiia@gmail.com>
2025-02-26 23:31:29 +0100

    Use string for param

--- xai/models/models.py
+++ xai/models/models.py
@@ -2,11 +2,10 @@ from xai.models.alexnet import AlexNet
 from xai.models.resnet18 import ResNet18
 from xai.models.vgg16 import VGG16
 from xai.models.vit import ViT
-from xai.utils.consts import MODEL_NAMES
 from xai.utils.logger import logger
 
 
-def get_model(model_name: MODEL_NAMES, num_classes: int):
+def get_model(model_name: str, num_classes: int):
     logger.info(f"Start loading model: {model_name}")
 
     match model_name:
