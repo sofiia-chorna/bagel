c8c717208b88827a0216c87ea8b6ffab1f691651
Sofiia Chorna <chernaya.sofiia@gmail.com>
2025-03-05 10:21:36 +0100

    Add confusion matrix command

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from datasets import Dataset, load_dataset
 from xai.concepts.concept_manager import concept_manager
 from xai.concepts.multilabel_classification import run_multilabel_clf
 from xai.datasets.datamodule import DataModule
+from xai.evaluation.confusion_matrix import (get_confusion_matrix,
+                                             plot_confusion_matrix)
 from xai.feature_extraction.feature_extraction import extract_features
 from xai.models.models import get_model
 from xai.utils.cli import path
@@ -31,14 +33,31 @@ def annotate(path: str):
     train_concepts_df = concept_manager.ask_llm(train_ds, params.batch_size)
     val_concepts_df = concept_manager.ask_llm(val_ds, params.batch_size)
 
-    # TODO: implement
-    # concept_df = concept_manager.extract_concepts(computation_params.dataset)
-
     logger.info(type(train_concepts_df))
     logger.info(train_concepts_df)
 
     save("pickle", f"concepts/{params.dataset_name}_train.pkl", train_concepts_df)
     save("pickle", f"concepts/{params.dataset_name}_val.pkl", val_concepts_df)
+
+
+@main.command()
+@path
+def confusion_matrix(path: str):
+    params = Params.from_yaml(path)
+    logger.info(f"Params used: {params.to_json()}")
+
+    # dataset
+    datamodule = DataModule(params.dataset_type, params.dataset_name, params.batch_size)
+
+    # calculate
+    for model_name in params.models:
+        model = get_model(model_name, datamodule.num_classes)
+        conf_matrix = get_confusion_matrix(model, datamodule.val_loader)
+        fig = plot_confusion_matrix(conf_matrix, datamodule.label_names, model_name)
+
+        dataset_name = params.dataset_name.split("/")[1]
+        save_path = f"results/confusion_matrix/{model_name}_{dataset_name}.png"
+        save("plt", save_path, fig)
 
 
 @main.command()
