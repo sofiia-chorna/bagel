0758f3fdae99eb280f7202f3adde69e5673288e8
Sofiia Chorna <chernaya.sofiia@gmail.com>
2025-03-19 01:42:08 +0100

    Delete refactorized function

--- xai/feature_extraction/feature_extraction.py
+++ xai/feature_extraction/feature_extraction.py
@@ -1,8 +1,6 @@
-from typing import Dict, List, Optional, Tuple
+from typing import Dict, List, Tuple
 
 import torch
-from pandas import DataFrame
-from sklearn.model_selection import train_test_split
 from torch import Tensor, nn
 from torch.utils.data import DataLoader
 from tqdm import tqdm
@@ -50,42 +48,3 @@ def extract_features(model: BaseModel, loader: DataLoader) -> Dict[str, Tensor]:
     logger.info(f"End extracting features")
 
     return final_features
-
-
-def train_test_split_features(
-    concepts_df: DataFrame,
-    model_features_dict: Dict[str, Dict[str, Tensor]],
-    dataset_name: Optional[str] = "dataset",
-):
-    train_df, val_df = train_test_split(
-        concepts_df, test_size=0.2, random_state=42, stratify=concepts_df["label"]
-    )
-
-    train_indices = set(train_df["index"].tolist())
-    val_indices = set(val_df["index"].tolist())
-
-    train_features = {}
-    val_features = {}
-
-    for model_name, features_dict in model_features_dict.items():
-        for layer_name, embeddings in features_dict.items():
-            train_features[layer_name] = embeddings[
-                [i for i in range(len(embeddings)) if i in train_indices]
-            ]
-            val_features[layer_name] = embeddings[
-                [i for i in range(len(embeddings)) if i in val_indices]
-            ]
-
-        save(
-            "torch",
-            f"features/{dataset_name}/{model_name}_features_train.pt",
-            train_features,
-        )
-        save(
-            "torch",
-            f"features/{dataset_name}/{model_name}_features_val.pt",
-            val_features,
-        )
-
-    save("pickle", f"xai/concepts/{dataset_name}_concepts_train.pkl", train_df)
-    save("pickle", "xai/concepts/{dataset_name}_concepts_val.pkl", val_df)
