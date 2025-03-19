59e32b8f93465f55b99b8662988fa2f2049d3350
Sofiia Chorna <chernaya.sofiia@gmail.com>
2025-03-19 01:40:16 +0100

    Imagenet label enumeration

--- xai/utils/consts.py
+++ xai/utils/consts.py
@@ -1,8 +1,21 @@
+import json
 from typing import Literal
 
 import torch
 
 MODEL_NAMES = Literal["alexnet", "resnet18", "vgg16"]
-DATASET_TYPES = Literal["hugging_face", "local"]
+DATASET_TYPES = Literal["hugging_face", "local", "imagenet"]
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+with open("xai/utils/imagenet_class_index.json", "r") as f:
+    imagenet_class_mapping = json.load(f)
+
+IMAGENET_CLASS_TO_LABEL = {
+    synset[0]: int(label) for label, synset in imagenet_class_mapping.items()
+}
+
+IMAGENET_LABEL_TO_NAME = {
+    int(label): synset[1] for label, synset in imagenet_class_mapping.items()
+}
