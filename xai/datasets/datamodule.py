b9fccc48a3a9e8cfa8ee1c0fa32b3e040c2f33fe
Sofiia Chorna <chernaya.sofiia@gmail.com>
2025-03-22 01:59:02 +0100

    Refactor to be hf dataloader

--- xai/datasets/datamodule.py
+++ xai/datasets/datamodule.py
@@ -1,33 +1,27 @@
 from torch.utils.data import DataLoader, Dataset
 
 from xai.datasets.hugging_face import HuggingFaceDataset
-from xai.utils.consts import DATASET_TYPES
 from xai.utils.logger import logger
 
 
-class DataModule:
+class HuggingFaceDataModule:
     train_loader: DataLoader
     val_loader: DataLoader
 
-    def __init__(self, dataset_type: DATASET_TYPES, dataset_name: str, batch_size: int):
+    def __init__(self, dataset_name: str, batch_size: int, from_disk: bool = False):
         super().__init__()
 
         logger.info(f"Start creating dataloader for: {dataset_name}")
 
-        if dataset_type in ["hugging_face", "local"]:
-            from_disk = dataset_type == "local"
-
-            train_dataset = HuggingFaceDataset(
-                dataset_name, split="train", from_disk=from_disk
-            )
-            val_dataset = HuggingFaceDataset(
-                dataset_name, split="validation", from_disk=from_disk
-            )
-            self.num_classes = train_dataset.get_num_classes()
-            self.label_mapping = train_dataset.get_label_mapping()
-            self.label_names = train_dataset.get_label_names()
-        else:
-            raise ValueError(f"Unknown dataset type: {dataset_type}")
+        train_dataset = HuggingFaceDataset(
+            dataset_name, split="train", from_disk=from_disk
+        )
+        val_dataset = HuggingFaceDataset(
+            dataset_name, split="validation", from_disk=from_disk
+        )
+        self.num_classes = train_dataset.get_num_classes()
+        self.label_mapping = train_dataset.get_label_mapping()
+        self.label_names = train_dataset.get_label_names()
 
         self.train_loader = self._get_dataloader(train_dataset, batch_size)
         self.val_loader = self._get_dataloader(val_dataset, batch_size)
