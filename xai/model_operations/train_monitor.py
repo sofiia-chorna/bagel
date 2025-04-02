627b0f5d440184148dc27354ad15479bf737133a
Sofiia Chorna <chernaya.sofiia@gmail.com>
2025-04-03 00:53:38 +0200

    Add train monitor

---
+++ xai/model_operations/train_monitor.py
@@ -0,0 +1,15 @@
+class TrainMonitor:
+    def __init__(self, patience: int = 5, min_delta: float = 0.01):
+        self.patience = patience
+        self.min_delta = min_delta
+        self.best_val = float("infinity")
+        self.counter = 0
+
+    def should_stop(self, val_loss: float) -> bool:
+        if val_loss < self.best_val - self.min_delta:
+            self.best_val = val_loss
+            self.counter = 0
+        else:
+            self.counter += 1
+
+        return self.counter >= self.patience
